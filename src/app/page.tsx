3e3c476dd44b31233009e3b177338b3cb82e7873
budiharta <budiharta_21@live.com>
2024-10-24 23:21:58 +0800

    update image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <div className="container mx-auto text-center">
             <div className="max-w-3xl mx-auto">
               <div className="relative h-screen flex items-center justify-center">
-                <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/main.jpg')] bg-cover bg-center bg-no-repeat opacity-40" />
+                <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/images/main.jpg')] bg-cover bg-center bg-no-repeat opacity-40" />
                 <div className="hidden md:block absolute top-0 left-0 bottom-0 w-[92px] bg-gradient-to-l from-transparent from-20% via-white to-white" />
                 <div className="hidden md:block absolute top-0 right-0 bottom-0 w-[92px] bg-gradient-to-r from-transparent from-20% via-white to-white" />
 
@@ -108,7 +108,7 @@ export default function Home() {
       </main>
 
       <footer className="bg-pink-800 text-white relative h-[320px] md:h-[520px] flex items-center justify-center w-full">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/footer.jpg')] bg-cover bg-center bg-no-repeat opacity-40" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/images/footer.jpg')] bg-cover bg-center bg-no-repeat opacity-40" />
         <div className="relative z-10 container mx-auto px-4 py-12">
           <div className="animate-on-scroll text-center mb-8 md:mb-0">
             <h2 className="text-5xl md:text-7xl font-bold mb-4 font-alex">
