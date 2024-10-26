cca1177717119335913957f7f55979cf056d876d
seriusanbudi <budiharta_21@live.com>
2024-10-26 08:41:28 +0800

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import { CalendarHeart, Clock, MapPin, Heart, Bike } from "lucide-react";
+"use client";
+
+import {
+  CalendarHeart,
+  Clock,
+  MapPin,
+  Heart,
+  TriangleAlert,
+} from "lucide-react";
 
 export default function Home() {
   return (
     <div className="min-h-dvh bg-gradient-to-b from-pink-100 to-white">
-      <header className="bg-white bg-opacity-90 shadow-2xl fixed top-0 left-0 right-0 z-50">
-        <div className="container mx-auto px-4 py-4 flex justify-center items-center">
-          <Heart className="w-6 h-6 md:w-8 md:h-8 mx-auto text-pink-600" />
-        </div>
-      </header>
-
       <main>
         <section id="home" className="bg-white relative shadow-2xl">
           <div className="container mx-auto text-center">
@@ -18,9 +20,13 @@ export default function Home() {
                 <div className="hidden md:block absolute top-0 left-0 bottom-0 w-[92px] bg-gradient-to-l from-transparent from-20% via-white to-white" />
                 <div className="hidden md:block absolute top-0 right-0 bottom-0 w-[92px] bg-gradient-to-r from-transparent from-20% via-white to-white" />
 
-                <div className="relative text-center z-10 px-4">
-                  <h1 className=" text-6xl lg:text-8xl font-bold mb-4 font-alex text-pink-600">
-                    Budi & Saras
+                <div className="relative text-center z-10 px-4 w-full md:w-[80%]">
+                  <h1 className=" text-6xl lg:text-8xl font-bold mb-4 font-alex text-pink-600 relative w-full py-6">
+                    <div className="z-10 relative whitespace-nowrap">
+                      Budi & Saras
+                    </div>
+
+                    <div className="w-full h-full bg-pink-300 absolute top-0 left-0 right-0 bottom-0 blur-3xl opacity-60" />
                   </h1>
                   <div className="flex justify-center">
                     <p className="bg-pink-600 text-base font-bold px-4 py-1 text-white rounded-md w-fit">
@@ -46,7 +52,7 @@ export default function Home() {
               dan kelanggengan ikatan kami.
             </p>
             <div className=" w-24 md:w-32 h-1 bg-pink-300 mx-auto mb-6 md:mb-8"></div>
-            <p className=" text-base md:text-lg lg:text-xl text-gray-600">
+            <p className="text-base md:text-lg lg:text-xl text-pink-500">
               Kehadiran Anda akan menjadikan hari istimewa kami semakin berkesan
               dan tak terlupakan.
             </p>
@@ -61,24 +67,24 @@ export default function Home() {
 
             <div className="grid gap-4 md:gap-8 w-full">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
-                <div className=" bg-white p-6 md:p-8 rounded-3xl shadow-lg text-center transform transition-transform duration-300">
+                <div className=" bg-white p-6 md:p-8 rounded-3xl text-center transform transition-transform duration-300">
                   <CalendarHeart className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-600" />
                   <h3 className="text-2xl md:text-3xl font-semibold mb-2 text-pink-600 font-alex">
                     Hari & Tanggal
                   </h3>
                   <p className="text-gray-700">Senin, 4 November 2024</p>
                 </div>
-                <div className=" bg-white p-6 md:p-8 rounded-3xl shadow-lg text-center transform transition-transform duration-300">
+                <div className=" bg-white p-6 md:p-8 rounded-3xl text-center transform transition-transform duration-300">
                   <Clock className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-600" />
                   <h3 className="text-2xl md:text-3xl font-semibold mb-2 text-pink-600 font-alex">
                     Waktu
                   </h3>
-                  <p className="text-gray-700">16:00 - 22:00</p>
+                  <p className="text-gray-700">16:00 WITA - 22:00 WITA</p>
                 </div>
               </div>
 
-              <div>
-                <div className=" bg-white p-6 md:p-8 rounded-3xl shadow-lg text-center transform transition-transform duration-300">
+              <div className="bg-white p-6 md:p-8 rounded-3xl text-center transform transition-transform duration-300 grid gap-4">
+                <div>
                   <MapPin className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-600" />
                   <h3 className="text-2xl md:text-3xl font-semibold mb-2 md:mb-4 text-pink-600 font-alex">
                     Lokasi
@@ -86,18 +92,31 @@ export default function Home() {
                   <p className="text-gray-700 mb-2">
                     Jalan Batuyang Gg. Garuda No. 100X
                   </p>
-                  <p className="text-gray-700 mb-6 font-bold">(Pondok Luwih)</p>
-
-                  <div className="mb-10">
-                    <iframe
-                      className="rounded-3xl shadow-xl h-[250px] lg:h-[450px]"
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126227.19759786579!2d115.14019833015911!3d-8.634350410933958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23fd7aff3b747%3A0xdd2e11b1ef510d7b!2sPondok%20Luwih!5e0!3m2!1sen!2sid!4v1729780742966!5m2!1sen!2sid"
-                      width="100%"
-                      loading="lazy"
-                      referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                  </div>
-
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://maps.app.goo.gl/gCKwop4cS8khAigq7"
+                    className="text-pink-600 font-bold underline"
+                  >
+                    (Pondok Luwih)
+                  </a>
+                </div>
+                <div className="p-4 bg-pink-100 rounded-3xl grid gap-2 border-2 border-pink-600 mb-2">
+                  <TriangleAlert className="w-8 h-8 mx-auto text-pink-600" />
+                  <p className="text-sm md:text-base text-gray-700 text-center">
+                    Lebih disarankan untuk membawa motor, karena mobil tidak
+                    bisa langsung masuk ke lokasi. Anda perlu berjalan sekitar
+                    500 meter dari tempat parkir.
+                  </p>
+                </div>
+                <div>
+                  <iframe
+                    className="rounded-3xl h-[250px] lg:h-[450px] mb-10"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126227.19759786579!2d115.14019833015911!3d-8.634350410933958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd23fd7aff3b747%3A0xdd2e11b1ef510d7b!2sPondok%20Luwih!5e0!3m2!1sen!2sid!4v1729780742966!5m2!1sen!2sid"
+                    width="100%"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
@@ -106,17 +125,6 @@ export default function Home() {
                   >
                     Lihat dalam peta
                   </a>
-                </div>
-
-                <div className="mt-4 p-4 bg-pink-100 rounded-3xl grid gap-4">
-                  <Bike className="w-8 h-8 mx-auto text-pink-600" />
-                  <p className="text-sm md:text-base text-gray-700 text-center">
-                    <b>Catatan:</b>
-                    <br />
-                    Lebih disarankan untuk membawa motor daripada mobil, karena
-                    mobil tidak bisa langsung masuk ke lokasi. Anda perlu
-                    berjalan sekitar 500 meter dari tempat parkir.
-                  </p>
                 </div>
               </div>
             </div>
@@ -130,7 +138,7 @@ export default function Home() {
           <div className=" text-center">
             <p className="font-alex text-5xl md:text-7xl">Terima kasih</p>
             <p className="text-xl md:text-2xl">
-              Atas kehadiran dan menjadi bagian dari kisah cinta kami.
+              Untuk menjadi bagian dari kisah cinta kami.
             </p>
           </div>
         </div>
