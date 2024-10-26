f81bac788a5a007376e21a531fae9ef51be899dd
seriusanbudi <budiharta_21@live.com>
2024-10-26 11:20:03 +0800

    fix layout continuity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
                 <div className="relative text-center z-10 w-full grid gap-4 py-4 lg:py-20">
                   <div className="w-full h-full bg-pink-300 absolute top-0 left-0 right-0 bottom-0 blur-3xl opacity-50" />
                   <div className="text-pink-500 tracking-[6px]">PERNIKAHAN</div>
-                  <div className="flex items-center justify-between gap-6 lg:gap-8 text-[16vw] lg:text-[8vw] font-bold font-alex text-pink-500 relative">
+                  <div className="flex flex-col md:flex-row items-center justify-between lg:gap-8 text-[16vw] lg:text-[8vw] font-bold font-alex text-pink-500 relative">
                     <div className="flex-1 text-right">Budi</div>
                     <div className="text-[5vw] lg:text-[3vw] flex-none">&</div>
                     <div className="flex-1 text-left">Saras</div>
@@ -39,9 +39,9 @@ export default function Home() {
           <div className="w-full h-[240px] bg-gradient-to-b from-transparent to-white absolute bottom-0 left-0 right-0" />
         </section>
         <section id="announcement" className="py-16 md:py-20 bg-white">
-          <div className="container mx-auto px-4 text-center">
+          <div className="mx-auto px-4 text-center">
             <Heart className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-6 md:mb-8 text-pink-500" />
-            <h2 className="text-4xl lg:text-6xl mb-6 md:mb-8 font-alex text-pink-500">
+            <h2 className="text-4xl lg:text-6xl mb-6 md:mb-8 font-alex text-pink-500 underline">
               We are Married!
             </h2>
             <p className="lg:text-2xl mb-6 md:mb-8 text-pink-700 max-w-3xl mx-auto">
@@ -58,19 +58,39 @@ export default function Home() {
             <div className="w-24 md:w-32 h-1 bg-pink-300 mx-auto"></div>
           </div>
         </section>
-        <section id="event" className="py-16 md:py-20 bg-pink-50">
-          <div className="container mx-auto px-4">
-            <h2 className="text-4xl lg:text-6xl text-center mb-12 md:mb-16 text-pink-500 font-alex">
+        <div className="border-b border-pink-500 opacity-25" />
+        <section id="event" className="py-16 md:py-20 bg-white">
+          <div className="mx-auto px-4 grid gap-4 md:gap-8">
+            <h2 className="text-4xl lg:text-6xl text-center text-pink-500 font-alex underline">
               Rincian Acara
             </h2>
-            <div className="grid gap-4 md:gap-8 w-full">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+            <div className="grid w-full">
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
                 <div className="bg-white p-6 md:p-8 rounded-3xl text-center">
                   <CalendarHeart className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-500" />
                   <h3 className="text-2xl md:text-3xl font-semibold mb-2 text-pink-500 font-alex">
                     Hari & Tanggal
                   </h3>
                   <p className="text-pink-700">Senin, 4 November 2024</p>
+                </div>
+                <div className="bg-white p-6 md:p-8 rounded-3xl text-center">
+                  <MapPin className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-500" />
+                  <h3 className="text-2xl md:text-3xl font-semibold mb-2 text-pink-500 font-alex">
+                    Lokasi
+                  </h3>
+                  <p className="text-pink-700">
+                    Jalan Batuyang Gg. Garuda No. 100X
+                  </p>
+                  <div className="flex justify-center">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="https://maps.app.goo.gl/gCKwop4cS8khAigq7"
+                      className="text-pink-500 font-bold underline"
+                    >
+                      (Pondok Luwih)
+                    </a>
+                  </div>
                 </div>
                 <div className="bg-white p-6 md:p-8 rounded-3xl text-center">
                   <Clock className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-500" />
@@ -81,23 +101,6 @@ export default function Home() {
                 </div>
               </div>
               <div className="bg-white p-6 md:p-8 rounded-3xl text-center grid gap-4">
-                <div>
-                  <MapPin className="w-12 h-12 md:w-16 md:h-16 mx-auto mb-4 md:mb-6 text-pink-500" />
-                  <h3 className="text-2xl md:text-3xl font-semibold mb-2 md:mb-4 text-pink-500 font-alex">
-                    Lokasi
-                  </h3>
-                  <p className="text-pink-700 mb-2">
-                    Jalan Batuyang Gg. Garuda No. 100X
-                  </p>
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://maps.app.goo.gl/gCKwop4cS8khAigq7"
-                    className="text-pink-500 font-bold underline"
-                  >
-                    (Pondok Luwih)
-                  </a>
-                </div>
                 <div className="p-4 bg-pink-100 rounded-3xl grid gap-2 border-2 border-pink-500 mb-2">
                   <TriangleAlert className="w-8 h-8 mx-auto text-pink-500" />
                   <p className="text-sm md:text-base text-pink-700 text-center">
@@ -129,8 +132,9 @@ export default function Home() {
         </section>
       </main>
       <footer className="bg-white text-white relative h-[320px] md:h-[520px] flex items-center justify-center w-full">
+        <div className="absolute top-0 left-0 right-0 h-[240px] z-20 bg-gradient-to-b from-white to-transparent" />
         <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/images/footer.jpg')] bg-cover bg-center bg-no-repeat opacity-20" />
-        <div className="relative z-10 container mx-auto px-4 py-12">
+        <div className="relative z-10 mx-auto px-4 py-12">
           <div className="text-center">
             <p className="font-alex text-5xl md:text-7xl text-pink-500">
               Terima kasih
