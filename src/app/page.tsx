1ec07e2b71f39541828e9bf455e02bb1a2fd29de
seriusanbudi <budiharta_21@live.com>
2024-10-26 11:29:24 +0800

    mobile responsive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,26 @@ export default function Home() {
           <div className="text-center">
             <div>
               <div className="relative min-h-dvh flex items-center justify-center">
+                <div className="block md:hidden absolute top-20 left-0 right-0 text-center text-pink-500 tracking-[6px]">
+                  PERNIKAHAN
+                </div>
+                <div className="flex md:hidden absolute bottom-20 left-0 right-0 justify-center z-20">
+                  <p className="border-2 border-pink-500 text-base font-bold px-4 py-1 text-pink-500 rounded-md w-fit bg-pink-300/50">
+                    4 November 2024
+                  </p>
+                </div>
+
                 <div className="relative text-center z-10 w-full grid gap-4 py-4 lg:py-20">
                   <div className="w-full h-full bg-pink-300 absolute top-0 left-0 right-0 bottom-0 blur-3xl opacity-50" />
-                  <div className="text-pink-500 tracking-[6px]">PERNIKAHAN</div>
-                  <div className="flex flex-col md:flex-row items-center justify-between lg:gap-8 text-[16vw] lg:text-[8vw] font-bold font-alex text-pink-500 relative">
+                  <div className="hidden md:block text-pink-500 tracking-[6px]">
+                    PERNIKAHAN
+                  </div>
+                  <div className="flex flex-col md:flex-row items-center justify-between lg:gap-8 text-[24vw] lg:text-[8vw] font-bold font-alex text-pink-500 relative leading-[15vw]">
                     <div className="flex-1 text-right">Budi</div>
-                    <div className="text-[5vw] lg:text-[3vw] flex-none">&</div>
+                    <div className="text-[8vw] lg:text-[3vw] flex-none">&</div>
                     <div className="flex-1 text-left">Saras</div>
                   </div>
-                  <div className="flex justify-center">
+                  <div className="hidden md:flex justify-center ">
                     <p className="border-2 border-pink-500 text-base font-bold px-4 py-1 text-pink-500 rounded-md w-fit bg-pink-300/50">
                       4 November 2024
                     </p>
